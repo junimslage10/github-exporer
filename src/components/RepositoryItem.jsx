ba48f06471e5658407f970c6c3c56ce48da8d089
Junior Lage <slage@MacBook-Pro-de-Junior.local>
2021-03-09 06:45:22 -0300

    Projeto Github Explorer - Ignite

---
+++ src/components/RepositoryItem.jsx
@@ -0,0 +1,11 @@
+export function RepositoryItem(){
+  return {
+    <li>
+          <strong>unform</strong>
+          <p>Forms in React</p>
+          <a href="">
+            Acessar repositório
+          </a>
+        </li>
+  };
+}
